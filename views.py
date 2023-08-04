ad886ccd7ae6c47d34cf13009e307afe2e2d2273
sofiamiranda28 <133670808+sofiamiranda28@users.noreply.github.com>
2023-08-04 01:41:51 -0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import RegistroForm, ContactoForm
+
+# Create your views here.
+
+class Registro(CreateView):
+    #Forms Django
+    form_class = RegistroForm
+    success_url = reverse_lazy('login')
+    template_name = 'usuarios/register.html'
+
+def contacto(request):
+    data = {
+        'form': ContactoForm()
+    }
+    if request.method == 'POST':
+        ContactoForm(data=request.POST).save()
+    
+    return render(request, 'contacto/formulario.html', data)
